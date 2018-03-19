1fdc27e47fa2c1cecaa8155c249c5148bf161759
ehayanis <hayani.mehdi149@gmail.com>
2018-03-19 11:49:40 +0100

    First Commit

---
+++ main.tf
@@ -0,0 +1,184 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+}
+
+resource "aws_vpc" "default" {
+    cidr_block = "${var.vpc_cidr}"
+    enable_dns_hostnames = true
+    tags {
+        Name = "caas-aws-vpc"
+    }
+}
+
+resource "aws_internet_gateway" "default" {
+    vpc_id = "${aws_vpc.default.id}"
+}
+
+/*
+  Public Subnet
+*/
+resource "aws_subnet" "us-east-1a-public" {
+    vpc_id = "${aws_vpc.default.id}"
+
+    cidr_block = "${var.public_subnet_cidr}"
+    availability_zone = "us-east-1a"
+
+    tags {
+        Name = "Public Subnet"
+    }
+}
+
+resource "aws_route_table" "us-east-1a-public" {
+    vpc_id = "${aws_vpc.default.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.default.id}"
+    }
+
+    tags {
+        Name = "Public Subnet"
+    }
+}
+
+resource "aws_route_table_association" "us-east-1a-public" {
+    subnet_id = "${aws_subnet.us-east-1a-public.id}"
+    route_table_id = "${aws_route_table.us-east-1a-public.id}"
+}
+
+/*
+  Private Subnet
+*/
+resource "aws_subnet" "us-east-1a-private" {
+    vpc_id = "${aws_vpc.default.id}"
+
+    cidr_block = "${var.private_subnet_cidr}"
+    availability_zone = "us-east-1a"
+
+    tags {
+        Name = "Private Subnet"
+    }
+}
+
+resource "aws_eip" "nat" {
+    vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = "${aws_eip.nat.id}"
+  subnet_id = "${aws_subnet.us-east-1a-public.id}"
+
+  depends_on = ["aws_internet_gateway.default"]
+}
+
+resource "aws_route_table" "us-east-1a-private" {
+    vpc_id = "${aws_vpc.default.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = "${aws_nat_gateway.nat.id}"
+    }
+
+    tags {
+        Name = "Private Subnet"
+    }
+}
+
+resource "aws_route_table_association" "us-east-1a-private" {
+    subnet_id = "${aws_subnet.us-east-1a-private.id}"
+    route_table_id = "${aws_route_table.us-east-1a-private.id}"
+}
+
+/*
+  BASTION Instance
+*/
+resource "aws_security_group" "bastion-sg" {
+    name = "caas_bastion"
+    description = "Allow incoming traffic through SSH"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    vpc_id = "${aws_vpc.default.id}"
+
+    tags {
+        Name = "BASTION_SG"
+    }
+}
+
+resource "aws_instance" "bastion" {
+    ami = "ami-1853ac65"
+    availability_zone = "us-east-1a"
+    instance_type = "t2.micro"
+    key_name = "${var.aws_key_name}"
+    vpc_security_group_ids = ["${aws_security_group.bastion-sg.id}"]
+    subnet_id = "${aws_subnet.us-east-1a-public.id}"
+    associate_public_ip_address = true
+    source_dest_check = false
+
+    tags {
+        Name = "CAAS BASTION"
+    }
+}
+
+resource "aws_eip" "bastion-ip" {
+    instance = "${aws_instance.bastion.id}"
+    vpc = true
+}
+
+resource "aws_security_group" "caas-sg" {
+    name = "caas_instances"
+    description = "Allow incoming traffic from public subnet through SSH"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["10.0.0.0/24"]
+    }
+
+    vpc_id = "${aws_vpc.default.id}"
+
+    tags {
+        Name = "CAAS_SG"
+    }
+}
+
+resource "aws_instance" "aad1" {
+    ami = "ami-1853ac65"
+    availability_zone = "us-east-1a"
+    instance_type = "t2.micro"
+    key_name = "${var.aws_key_name}"
+    vpc_security_group_ids = ["${aws_security_group.caas-sg.id}"]
+    subnet_id = "${aws_subnet.us-east-1a-private.id}"
+    associate_public_ip_address = true
+    source_dest_check = false
+
+    tags {
+        Name = "CAAS_AAD"
+    }
+}
+
+output "ip" {
+  value = "${aws_eip.bastion-ip.public_ip}"
+}
