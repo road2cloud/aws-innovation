40d1e6d049055872722a10e10cc42254632e58e7
ehayanis <hayani.mehdi149@gmail.com>
2018-08-17 11:52:33 +0200

    add cidr for 2 subnets

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -3,12 +3,22 @@ variable "vpc_cidr" {
     default = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidr" {
-    description = "CIDR for the Public Subnet"
+variable "public_subnet_cidr_1" {
+    description = "CIDR for the Public Subnet 1"
     default = "10.0.0.0/24"
 }
 
-variable "private_subnet_cidr" {
-    description = "CIDR for the Private Subnet"
+variable "public_subnet_cidr_2" {
+    description = "CIDR for the Public Subnet 2"
     default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+    description = "CIDR for the Private Subnet 1"
+    default = "10.0.2.0/24"
+}
+
+variable "private_subnet_cidr" {
+    description = "CIDR for the Private Subnet 2"
+    default = "10.0.3.0/24"
 }
